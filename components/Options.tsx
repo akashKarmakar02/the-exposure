63de1fb4cb8affdd982961643d626e8abd1fdf39
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-17 22:33:19 +0530

    Options component to filter images in gallert by category

---
+++ components/Options.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Options({ categories }: any) {
+  const navigator = useRouter();
+  let [seleted, setSelected] = useState("/gallery");
+
+  return (
+    <div className="flex justify-end mb-2 mx-10">
+      <div>
+        <label
+          htmlFor="filter"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Filter
+        </label>
+        <select
+          id="filter"
+          onChange={(e) => {
+            setSelected(e.target.value);
+            navigator.push(e.target.value);
+          }}
+          defaultValue={seleted}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+          <option value={"/gallery"}>All</option>
+          {categories.map((category: any) => (
+            <option
+              key={category._id}
+              value={`/gallery/${category.slug.current}`}
+            >
+              {category.title}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
