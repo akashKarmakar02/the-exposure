2240450850451dfd858e990a6681ddf677734315
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-17 22:49:48 +0530

    removed unnecessary console logs

--- app/(user)/gallery/page.tsx
+++ app/(user)/gallery/page.tsx
@@ -1,8 +1,8 @@
+import Options from "@/components/Options";
 import { client } from "@/sanity/lib/client";
 import { urlForImage } from "@/sanity/lib/image";
 import { groq } from "next-sanity";
 import Image from "next/image";
-import Link from "next/link";
 
 const query = groq`
   *[_type=="post"] {
@@ -12,33 +12,38 @@ const query = groq`
   }
 `;
 
+const categoryQuery = groq`
+  *[_type=="category"] {
+    ...,
+  }
+`;
+
 export default async function gallery() {
   const posts = await client.fetch(query);
-  console.log(posts);
+  const categories = await client.fetch(categoryQuery);
 
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
-      {posts.map((post: any) => {
-        console.log(post.mainImage.asset);
-        return (
-          <div
-            key={post._id}
-            className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out"
-          >
-            <Image
-              alt="Next.js Conf photo"
-              className="transform sm:brightness-100 rounded-lg md:brightness-90 transition will-change-auto hover:brightness-100 hover:drop-shadow-lg cursor-zoom-in"
-              style={{ transform: "translate3d(0, 0, 0)" }}
-              src={urlForImage(post.mainImage.asset).url()}
-              sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-              fill
-            />
-          </div>
-        );
-      })}
+    <div>
+      <Options categories={categories} />
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
+        {posts.map((post: any) => {
+          return (
+            <div
+              key={post._id}
+              className="flex justify-center w-fit h-fit drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out"
+            >
+              <Image
+                alt="Next.js Conf photo"
+                className="transform  sm:brightness-100 rounded-lg md:brightness-90 transition will-change-auto hover:brightness-100 hover:drop-shadow-lg cursor-zoom-in"
+                style={{ transform: "translate3d(0, 0, 0)" }}
+                src={urlForImage(post.mainImage.asset).url()}
+                height={720}
+                width={1200}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
