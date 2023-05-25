34cc29c5949e32564a9bf9b11e367d0e63bb4dc0
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-25 09:09:58 +0530

    Moved it for more clean project structure and added Hero widget

---
+++ app/(user)/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import AnimatedText from "@/components/AnimatedText";
+
+export default function Home() {
+  return (
+      <article className={"px-10 py-20"}>
+          <section className="space-y-5 mb-4 border border-[#f7550ae9] text-white">
+              <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+                  <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+                      <Image
+                          className="object-cover object-center mx-auto"
+                          src="/owner.jpg"
+                          alt="owner"
+                          fill
+                      />
+                  </div>
+                  <section className="p-5 bg-[#f7550ae9] w-full">
+                      <div className="flex flex-col md:flex-row justify-between gap-y-5">
+                          <div>
+                              <Image
+                                  src="/owner.jpg"
+                                  alt="owner"
+                                  height={240}
+                                  width={480}
+                              />
+                          </div>
+                          <div className="flex items-center space-x-2">
+                              <AnimatedText />
+                          </div>
+                      </div>
+                  </section>
+              </div>
+          </section>
+      </article>
+  );
+}
